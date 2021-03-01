d8a733e96c8e284d471df070dadfaf470390a577
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-03-01 11:14:24 +0800

    bouns variable changed

--- Lab1/lab1_bonus.v
+++ Lab1/lab1_bonus.v
@@ -1,13 +1,16 @@
 module comparator(
-    input [2:0] a,
-    input [2:0] b,
-    output [2:0] o,
+    input a0,a1,a2;
+    input b0,b1,b2;
+    output o0,o1,o2;
     wire [2:0]x,
     wire flag
 );
-    assign x[0] = ~(a[0] ^ b[0]);
-    assign x[1] = ~(a[1] ^ b[1]);
-    assign x[2] = ~(a[2] ^ b[2]);
-    assign flag = ( x[0]&x[1]&x[2] ) + ( x[2]&x[1]&a[0]&(~b[0]) ) + ( x[2]&a[1]&(~b[1]) ) + ( a[2]&(~b[2]) );
-    assign o = flag? b:a;
+    assign x[0] = ~(a0 ^ b0);
+    assign x[1] = ~(a1 ^ b1);
+    assign x[2] = ~(a2 ^ b2);
+    // flag = 1 if A>=B
+    assign flag = ( x0&x1&x2 ) + ( x2&x1&a0&(~b0) ) + ( x2&a1&(~b1) ) + ( a2&(~b2) );
+    assign o0 = flag? b0:a0;
+    assign o1 = flag? b1:a1;
+    assign o2 = flag? b2:a2;
 endmodule
