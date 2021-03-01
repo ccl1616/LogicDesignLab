84312d678844f411400ee567635f5f45c5220476
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-03-01 11:43:27 +0800

    create snapshot folder

--- Lab1/lab1_bonus.v
+++ Lab1/lab1_bonus.v
@@ -1,7 +1,7 @@
 module comparator(
-    input a0,a1,a2;
-    input b0,b1,b2;
-    output o0,o1,o2;
+    input a0,a1,a2,
+    input b0,b1,b2,
+    output o0,o1,o2,
     wire [2:0]x,
     wire flag
 );
@@ -9,7 +9,7 @@ module comparator(
     assign x[1] = ~(a1 ^ b1);
     assign x[2] = ~(a2 ^ b2);
     // flag = 1 if A>=B
-    assign flag = ( x0&x1&x2 ) + ( x2&x1&a0&(~b0) ) + ( x2&a1&(~b1) ) + ( a2&(~b2) );
+    assign flag = ( x[0]&x[1]&x[2] ) + ( x[2]&x[1]&a0&(~b0) ) + ( x[2]&a1&(~b1) ) + ( a2&(~b2) );
     assign o0 = flag? b0:a0;
     assign o1 = flag? b1:a1;
     assign o2 = flag? b2:a2;
