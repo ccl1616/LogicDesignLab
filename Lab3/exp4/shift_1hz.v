adcedbf01c057c502840bd4b5d97d0f1758a5069
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-03-20 23:13:51 +0800

    lab3_4

---
+++ Lab3/exp4/shift_1hz.v
@@ -0,0 +1,28 @@
+// 1Hz 8bit shift
+
+`define BIT_WIDTH 8
+module shift_1hz(
+    q,
+    clk, //global clock
+    rst_n
+);
+output [`BIT_WIDTH-1 : 0] q;
+input clk;
+input rst_n;
+
+wire clk_div;
+wire [`CNT_BIT_WIDTH-1 : 0]q;
+
+freqdiv_50 U0(
+    .ans(clk_div), // divided clock
+    .clk(clk), // global clock, f crystal
+    .rst_n(rst_n)
+);
+
+shift U1(
+    .q(q),
+    .clk(clk_div),
+    rst_n(rst_n)
+);
+
+endmodule
