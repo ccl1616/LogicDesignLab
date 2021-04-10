fa66ff7099a03ba154401b2ed7b8bb8587812eb6
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-04-10 11:41:47 +0800

    press update

--- Lab5/2_long_press/press.v
+++ Lab5/2_long_press/press.v
@@ -1,34 +1,39 @@
-`include "golbal.v"
+`include "global.v"
 
 module press(
-    mode,
+    mode_in,
     short_press, 
     long_press,
     rst_n,
     clk
 );
-reg mode;
+input mode_in;
+input rst_n;
+input clk;
+output short_press;
+output long_press;
+reg pb_in;
 reg mode_delay;
 reg [1:0]press_count_next;
 reg [1:0]press_count;
 
 // short press
-assign short_press = (~mode)&mode_delay&( ~(press_count[1]&press_count[0]) );
+assign short_press = (~mode_in)&mode_delay&( ~(press_count[1]&press_count[0]) );
 always @(posedge clk or negedge rst_n) begin
     if(~rst_n)
         mode_delay <= `FALSE;
     else 
-        mode_delay <= mode;
+        mode_delay <= mode_in;
 end
 
 // long press
 // combinational
-assign long_press = (press_count == 2'b11);
+assign long_press = (press_count == 2'b11)&(mode_in==1'b1);
 always @* begin
-    if(mode == `FALSE)
+    if(mode_in == `FALSE)
         press_count_next = 2'd0;
     else 
-        press_count_next = press_count + 2'd1;
+        press_count_next = press_count + ( (press_count==2'd3)?2'd0:2'd1 );
 end
 
 // sequential
@@ -40,3 +45,4 @@ always @(posedge clk or negedge rst_n) begin
 end
 
 endmodule
+
