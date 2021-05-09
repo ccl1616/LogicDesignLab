b7f1daa70e5716b4013c5b1b57521a7318bce31d
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-05-09 16:37:05 +0800

    lab9_2 display

--- Lab9/lab9_2/display.v
+++ Lab9/lab9_2/display.v
@@ -1,28 +1,29 @@
 `timescale 1ns / 1ps
 `include "global.v"
 module display(
-	input [7:0] AS_letter,
+	input [3:0]value,
   	output reg [7:0]segs, // 14-segment segs output
 );
 
 // Combinatioanl Logic
 always@(*)
-  case(AS_letter)
-    8'd99 : segs = 8'b0110_0011; // lower c
-    8'd100 : num = 8'b1000_0101; // lower d
-    8'd101 : num = 8'b0110_0001; // lower e
-    8'd102 : num = 8'b0111_0001; // lower f
-    8'd103 : num = 8'b0100_0001; // lower g
-    8'd97 : num = 8'b0001_0001; // lower a
-    8'd98 : num = 8'b1100_0001; // lowerb
-    8'd67 : num = 8'b0110_0010; // cap c
-    8'd68 : num = 8'b1000_0100; // cap d
-    8'd69 : num = 8'b0110_0000; // cap e
-    8'd70 : num = 8'b0111_0000; // cap f
-    8'd71 : num = 8'b0100_0000; // cap g
-    8'd65 : num = 8'b0001_0000; // cap a
-    8'd66 : num = 8'b1100_0000; // lowerb
-    default : num = 8'b0110_0011; // lower c
+  case(value)
+    4'd0 : segs = 8'b0110_0011; // lower c
+    4'd1 : segs = 8'b1000_0101; // lower d
+    4'd2 : segs = 8'b0110_0001; // lower e
+    4'd3 : segs = 8'b0111_0001; // lower f
+    4'd4 : segs = 8'b0100_0001; // lower g
+    4'd5 : segs = 8'b0001_0001; // lower a
+    4'd6 : segs = 8'b1100_0001; // lowerb
+    4'd7 : segs = 8'b0110_0010; // cap c
+    4'd8 : segs = 8'b1000_0100; // cap d
+    4'd9 : segs = 8'b0110_0000; // cap e
+    4'd10 : segs = 8'b0111_0000; // cap f
+    4'd11 : segs = 8'b0100_0000; // cap g
+    4'd12 : segs = 8'b0001_0000; // cap a
+    4'd13 : segs = 8'b1100_0000; // lowerb
+	4'd15 : segs = 8'b1111_1111; // none
+    default : segs = 8'b0110_0011; // lower c
   endcase
 
 endmodule
